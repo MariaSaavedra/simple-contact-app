e968a29423f8f1cadcc1c2e35f9d2cdaf57f6692
Saavedra, Maria <msaavedra@epiqsystems.com>
2015-10-08 14:24:45 -0500

    Added in angular.js to project.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html ng-app="app">
    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -9,8 +9,8 @@
         <link rel="stylesheet" href="assets/css/styles.css">
     </head>
     <body>
-        
         <script src="node_modules/lodash/index.js"></script>
         <script src="node_modules/angular/angular.js"></script>
+        <script src="app/app.js"></script>
     </body>
 </html>
